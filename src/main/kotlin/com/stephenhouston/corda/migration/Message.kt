d3cafaac42aefbc08166133c4920e1e4c2dbdc0d
Stephen Houston <stephenjhouston@gmail.com>
2019-03-14 21:16:59 +0000

    Fix from Ivan, in order for the migration to be found, it must be a MappedSchema

--- src/main/kotlin/com/stephenhouston/corda/migration/Message.kt
+++ src/main/kotlin/com/stephenhouston/corda/migration/Message.kt
@@ -1,8 +1,18 @@
 package com.stephenhouston.corda.migration
 
+import net.corda.core.schemas.MappedSchema
+import net.corda.core.serialization.CordaSerializable
 import java.util.UUID
+
+object MessageSchema
+
+@CordaSerializable
+object MessageSchemaV1 : MappedSchema(
+    schemaFamily = MessageSchema.javaClass,
+    version = 1,
+    mappedTypes = listOf(Message::class.java))
 
 data class Message(
     val id : UUID,
-    val contents: String
+    val contents : String
 )
